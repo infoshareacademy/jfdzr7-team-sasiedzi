deeb92babb4a4f4b5e0dfa5d9a562e62ff7dd442
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2022-09-11 09:33:18 +0200

    images added as components

---
+++ neighbors/src/components/loginImage.jsx
@@ -0,0 +1,10 @@
+import LogImg from '../img/login.svg';
+import './mainLayout.css';
+
+export const LoginImage = () => {
+  return (
+    <>
+      <img src={LogImg} className="image" alt="login" />
+    </>
+  );
+};
---
+++ neighbors/src/components/registerImage.jsx
@@ -0,0 +1,10 @@
+import RegImg from '../img/register.svg';
+import './mainLayout.css';
+
+export const RegisterImage = () => {
+  return (
+    <>
+      <img src={RegImg} className="image" alt="register" />
+    </>
+  );
+};
