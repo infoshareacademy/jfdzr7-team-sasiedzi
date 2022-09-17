25fbfac0480a289ef8b4fc2e405d67e908dcf003
slawomir.miller <96744586+Slawek12m@users.noreply.github.com>
2022-09-17 09:22:43 +0200

    button display repair
    convert Link to NavLink

--- neighbors/src/components/navbar/Navbar.jsx
+++ neighbors/src/components/navbar/Navbar.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 export const Navbar = () => {
   return (
     <div className={styles.wrapper}>
-      <Link className={styles.link1} to={`/help`}>
+      <NavLink className={styles.link1} to={`/help`}>
         Chcę pomóc
-      </Link>
-      <Link className={styles.link} to={`/need`}>
+      </NavLink>
+      <NavLink className={styles.link} to={`/need`}>
         Potrzebuję pomocy
-      </Link>
+      </NavLink>
       <Link className={styles.link} to={`/how`}>
         Jak to działa?
       </Link>
-      <button className={styles.btn}>
-        <Link to={`/profil`}>Profil</Link>
-      </button>
-      <button className={styles['btn-2']}>
-        <Link to={`/logout`}>wyloguj się</Link>
-      </button>
+
+      <Link className={styles.btn} to={`/profil`}>
+        Profil
+      </Link>
+
+      <Link className={styles['btn-2']} to={`/logout`}>
+        wyloguj się
+      </Link>
     </div>
   );
 };
