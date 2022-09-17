da6bf068e3368c2e33996f6fe3394326417178b9
Cezary Chmura <czerag@o2.pl>
2022-09-17 12:59:33 +0200

    Formating fixes

--- neighbors/src/App.jsx
+++ neighbors/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { Navbar } from './components/navbar/Navbar';
 
 export const App = () => {
--- neighbors/src/components/navbar/Navbar.jsx
+++ neighbors/src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link, NavLink } from 'react-router-dom';
+
 import styles from './Navbar.module.css';
 
 export const Navbar = () => {
