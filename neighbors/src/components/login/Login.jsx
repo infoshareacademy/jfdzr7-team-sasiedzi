9ea50091088f0d70c026d93d429b1ea6c08d0f1c
Cezary Chmura <czerag@o2.pl>
2022-09-18 11:36:26 +0200

    LOGIN FORM FIXES

--- neighbors/src/components/login/Login.jsx
+++ neighbors/src/components/login/Login.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 import '../mainLayout.css';
 
 export function Login() {
@@ -6,11 +8,11 @@ export function Login() {
       <form action="" className="sign-in-form">
         <h2 className="title">Sign in</h2>
         <div className="input-field">
-          <i className="fas fa-user"></i>
+          <FontAwesomeIcon className="icon" icon={faUser} />
           <input type="text" placeholder="Username" />
         </div>
         <div className="input-field">
-          <i className="fas fa-lock"></i>
+          <FontAwesomeIcon className="icon" icon={faLock} />
           <input type="password" placeholder="Password" />
         </div>
         <input type="submit" value="Login" className="btn solid" />
