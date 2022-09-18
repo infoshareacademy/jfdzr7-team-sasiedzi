6b866348297e7a7c5ccde66e7c3f94c3199a0cfc
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2022-09-18 11:20:04 +0200

    tydtuły kafelków

--- neighbors/src/components/homePanel/HomePanel.jsx
+++ neighbors/src/components/homePanel/HomePanel.jsx
@@ -4,22 +4,22 @@ export const HomePanel = () => {
   const slides = [
     {
       image: 'https://picsum.photos/200/300',
-      title: 'This is a title',
+      title: 'Ogłoszenia',
       description: 'This is a description',
     },
     {
       image: 'https://picsum.photos/600/500',
-      title: 'This is a second title',
+      title: 'Dodaj ogłoszenie',
       description: 'This is a second description',
     },
     {
       image: 'https://picsum.photos/500/600',
-      title: 'This is a third title',
+      title: 'Profil',
       description: 'This is a third description',
     },
     {
       image: 'https://picsum.photos/500/300',
-      title: 'This is a fourth title',
+      title: 'Help',
       description: 'This is a fourth description',
     },
   ];
