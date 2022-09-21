3a285525c085e84f7e2e02e8847d4aa388e7759e
slawomir.miller <96744586+Slawek12m@users.noreply.github.com>
2022-09-21 21:36:11 +0200

    feature-SSIED-12-HomePanel-TEMP

--- neighbors/src/App.jsx
+++ neighbors/src/App.jsx
@@ -1,11 +1,13 @@
 import { Navbar } from './components/navbar/Navbar';
 import { Container } from './components/containers/container';
+import { HomePanel } from './components/homePanel/HomePanel';
 
 export const App = () => {
   return (
     <>
       <Navbar />
       <Container />
+      <HomePanel />
     </>
   );
 };
--- neighbors/src/components/homePanel/HomePanel.jsx
+++ neighbors/src/components/homePanel/HomePanel.jsx
@@ -1,45 +1,52 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComments } from '@fortawesome/free-solid-svg-icons';
 import './stylesHomePanel.css';
 
 export const HomePanel = () => {
   const slides = [
     {
-      image: 'https://picsum.photos/200/300',
+      // image: 'https://picsum.photos/200/300',
       title: 'Ogłoszenia',
       description: 'This is a description',
     },
     {
-      image: 'https://picsum.photos/600/500',
+      // image: 'https://picsum.photos/600/500',
       title: 'Dodaj ogłoszenie',
       description: 'This is a second description',
     },
     {
-      image: 'https://picsum.photos/500/600',
+      // image: 'https://picsum.photos/500/600',
       title: 'Profil',
       description: 'This is a third description',
     },
     {
-      image: 'https://picsum.photos/500/300',
+      // image: 'https://picsum.photos/500/300',
       title: 'Help',
       description: 'This is a fourth description',
     },
   ];
 
   return (
-    <div id="body">
-      <div id="main-container">
-        <div id="slider">
+    <div id='body'>
+      <div id='main-container'>
+        <div id='slider'>
           {slides.map((slide, index) => {
             return (
-              <div className="slider-card" key={index}>
+              <div className='slider-card' key={index}>
                 <div
-                  className="slider-card-image"
-                  style={{
-                    backgroundImage: `url(${slide.image})`,
-                    backgroundSize: 'cover',
-                  }}
-                ></div>
-                <p className="slider-card-title">{slide.title}</p>
-                <p className="slider-card-description">{slide.description}</p>
+                // className='slider-card-image'
+                // style={{
+                //   backgroundImage: `url(${slide.image})`,
+                //   backgroundSize: 'cover',
+                // }}
+                >
+                  <div className='left'>
+                    <FontAwesomeIcon className='icon' icon={faComments} />
+                  </div>
+                </div>
+
+                <p className='slider-card-title'>{slide.title}</p>
+                <p className='slider-card-description'>{slide.description}</p>
               </div>
             );
           })}
