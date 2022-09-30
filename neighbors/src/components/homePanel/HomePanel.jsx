389a60afff80f19fafba9d20cedbb4cffc670f3c
slawomir.miller <96744586+Slawek12m@users.noreply.github.com>
2022-09-30 19:31:25 +0200

    HomePanel-blad-skalowania

--- neighbors/src/components/homePanel/HomePanel.jsx
+++ neighbors/src/components/homePanel/HomePanel.jsx
@@ -1,52 +1,56 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComments } from '@fortawesome/free-solid-svg-icons';
 import './stylesHomePanel.css';
+import { NavLink } from 'react-router-dom';
+
+import addPost from './img/addPost.svg';
+import help from './img/help.svg';
+import posts from './img/posts.svg';
+import profile from './img/profile.svg';
+// import { ThemeContext, needHelpPostsData, offerHelpPostsData } from '../../helpers/apiCommunication';
 
 export const HomePanel = () => {
   const slides = [
     {
-      // image: 'https://picsum.photos/200/300',
+      link: `/help`,
+      svg: posts,
       title: 'Ogłoszenia',
       description: 'This is a description',
     },
     {
-      // image: 'https://picsum.photos/600/500',
+      svg: addPost,
       title: 'Dodaj ogłoszenie',
       description: 'This is a second description',
     },
     {
-      // image: 'https://picsum.photos/500/600',
+      svg: profile,
       title: 'Profil',
       description: 'This is a third description',
     },
     {
-      // image: 'https://picsum.photos/500/300',
+      svg: help,
       title: 'Help',
       description: 'This is a fourth description',
     },
   ];
 
   return (
-    <div id='body'>
-      <div id='main-container'>
-        <div id='slider'>
+    <div className="home-container">
+      <div id="body">
+        {/* <div className='container'>
+        <div className='bg-gradient'></div>
+      </div> */}
+
+        <div id="slider">
           {slides.map((slide, index) => {
             return (
-              <div className='slider-card' key={index}>
-                <div
-                // className='slider-card-image'
-                // style={{
-                //   backgroundImage: `url(${slide.image})`,
-                //   backgroundSize: 'cover',
-                // }}
-                >
-                  <div className='left'>
-                    <FontAwesomeIcon className='icon' icon={faComments} />
-                  </div>
-                </div>
+              <div className="slider-card" key={index}>
+                <div className="post-box">
+                  <img src={slide.svg} className="box-logo" alt="addPost Logo" />
 
-                <p className='slider-card-title'>{slide.title}</p>
-                <p className='slider-card-description'>{slide.description}</p>
+                  <p className="slider-card-title">{slide.title}</p>
+                  <p className="slider-card-description">{slide.description}</p>
+                </div>
               </div>
             );
           })}
