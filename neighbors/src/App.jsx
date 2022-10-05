6a2ed78ba78476b8966cae2478523745df374c4d
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2022-10-05 13:38:02 +0200

    app jsx z develp oraz route dodany do branchy

--- neighbors/src/App.jsx
+++ neighbors/src/App.jsx
@@ -8,21 +8,34 @@ import { UserContext } from './helpers/apiCommunication';
 import { AddPost } from './components/AddPost/AddPost';
 import { PostList } from './components/posts/post-list';
 import { PostDetails } from './components/posts/single-post';
+import { About } from './components/about/about';
 export const App = () => {
-  const { user } = useContext(UserContext);
-  // if (!user) {
-  //   return (
-  //     <>
-  //       <Container />
-  //     </>
-  //   );
-  // }
+  const { isAuth } = useContext(UserContext);
   return (
     <>
       <Navbar />
-      <AddPost />
       <Routes>
-        <Route path="/post/:id" element={<PostDetails />} />
+        {isAuth ? (
+          <>
+            <Route path="/" element={<HomePanel />} />
+            <Route path="/help-board" element={<PostList />} />
+            <Route path="/need-help" element={<AddPost />} />
+            <Route path="/post/:id" element={<PostDetails />} />
+            <Route path="/about" element={<About />} />
+          </>
+        ) : (
+          <Route path="/" element={<Container />} />
+        )}
+
+        <Route
+          path="*"
+          element={
+            <div className="container t-center ">
+              <h1 className="header-1 p-50">404 - Page not found</h1>
+              <p>This page does not exist or requires to log in.</p>
+            </div>
+          }
+        />
       </Routes>
     </>
   );
