09253c433657e82d1c468122883abe2333a797b8
Anna Kierlin <ania.kierlin@gmail.com>
2022-10-07 11:00:33 +0200

    logika do edycji zrobiona

--- neighbors/src/components/UserProfile/UserProfile.jsx
+++ neighbors/src/components/UserProfile/UserProfile.jsx
@@ -6,32 +6,115 @@ import { usersData } from '../../helpers/apiCommunication';
 import { auth } from '../../api/firebase';
 export const UserProfile = () => {
   const [profileData, setProfileData] = useState(null);
-  console.log(auth.currentUser.uid);
+  const [edit, setEdit] = useState(false);
+  const onClickEdit = () => {
+    setEdit(true);
+  };
+  const onClickCancel = () => {
+    setEdit(false);
+  };
+  const onClickSubmit = () => {
+    setEdit(false);
+  };
+  const onChange = () => {};
   useEffect(() => {
     onSnapshot(query(usersData, where(documentId(), '==', `${auth.currentUser.uid}`)), (querySnapshot) => {
       querySnapshot.docs.map((element) => {
-        console.log(element.data());
         setProfileData({ id: element.id, ...element.data() });
       });
     });
   }, []);
-  console.log(profileData);
   return (
     <>
-      git
       {profileData ? (
         <>
           <h1>Your Profile!</h1>
-          <p>First name: {profileData.firstName}</p>
-          <p>Last name: {profileData.lastName}</p>
-          <p>Contact: {profileData.phoneNumber}</p>
-          <p>City: {profileData.city}</p>
-          <p>Street: {profileData.street}</p>
-          <p>House number: {profileData.houseNumber ? <>{profileData.houseNumber}</> : <>Not given</>}</p>
-          <button> Edit your data</button>
+          {edit ? (
+            <>
+              <form>
+                <div>
+                  <label htmlFor="firstName">First name: </label>
+                  <input
+                    className="input-field"
+                    placeholder={profileData.firstName}
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    onChange={onChange}
+                  ></input>
+                </div>
+                <div>
+                  <label htmlFor="lastName">Last name: </label>
+                  <input
+                    className="input-field"
+                    type="text"
+                    placeholder={profileData.lastName}
+                    id="lastName"
+                    name="lastName"
+                    onChange={onChange}
+                  ></input>
+                </div>
+                <div>
+                  <label htmlFor="city">City: </label>
+                  <input
+                    className="input-field"
+                    type="text"
+                    id="city"
+                    placeholder={profileData.city}
+                    name="city"
+                    onChange={onChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="street">Street: </label>
+                  <input
+                    className="input-field"
+                    placeholder={profileData.street}
+                    type="text"
+                    id="street"
+                    name="street"
+                    onChange={onChange}
+                  />
+                  <label htmlFor="houseNumber">No.: </label>
+                  <input
+                    className="input-field-short"
+                    placeholder={profileData.houseNumber ? profileData.houseNumber : 'x'}
+                    type="text"
+                    id="houseNumber"
+                    name="houseNumber"
+                    onChange={onChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="phoneNumber">Phone number: </label>
+                  <input
+                    className="input-field"
+                    type="text"
+                    placeholder={profileData.phoneNumber}
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    onChange={onChange}
+                  />
+                </div>
+                <button onClick={onClickSubmit}>Submit changes </button>
+                <button onClick={onClickCancel}>Cancel edit </button>
+              </form>
+            </>
+          ) : (
+            <>
+              {' '}
+              <button onClick={onClickEdit}> Edit your data</button>
+              <p>First name: {profileData.firstName}</p>
+              <p>Last name: {profileData.lastName}</p>
+              <p>Contact: {profileData.phoneNumber}</p>
+              <p>City: {profileData.city}</p>
+              <p>Street: {profileData.street}</p>
+              <p>House number: {profileData.houseNumber ? <>{profileData.houseNumber}</> : <>Not given</>}</p>
+            </>
+          )}
         </>
       ) : (
-        <>Loadin Data</>
+        <>Loading Data</>
       )}
     </>
   );
