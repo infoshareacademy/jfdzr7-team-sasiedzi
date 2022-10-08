eda5913f10aeff73233e4e1fab4ccc3c65993ede
Anna Kierlin <ania.kierlin@gmail.com>
2022-10-08 11:59:13 +0200

    naprawy po zmergowanych zmianach

--- neighbors/src/App.jsx
+++ neighbors/src/App.jsx
@@ -8,8 +8,6 @@ import { UserContext } from './helpers/apiCommunication';
 import { AddPost } from './components/AddPost/AddPost';
 import { PostList } from './components/posts/post-list';
 import { PostDetails } from './components/posts/single-post';
-import { UserProfile } from './components/UserProfile/UserProfile';
-
 import { Footer } from './components/footer/footer';
 import { About } from './components/about/about';
 import { UserProfile } from './components/UserProfile/UserProfile';
@@ -17,37 +15,35 @@ export const App = () => {
   const { isAuth } = useContext(UserContext);
   return (
     <>
-
       <Navbar />
-      <UserProfile></UserProfile>
-      { <Routes>
-        {isAuth ? (
-          <>
-            <Route path="/" element={<HomePanel />} />
-            <Route path="/help-board" element={<PostList />} />
-            <Route path="/need-help" element={<AddPost />} />
-            <Route path="/post/:id" element={<PostDetails />} />
 
-            <Route path="/how" element={<About />} />
-          </>
-        ) : (
-          <Route path="/" element={<Container />} />
-        )}
+      {
+        <Routes>
+          {isAuth ? (
+            <>
+              <Route path="/" element={<HomePanel />} />
+              <Route path="/help-board" element={<PostList />} />
+              <Route path="/need-help" element={<AddPost />} />
+              <Route path="/post/:id" element={<PostDetails />} />
+              <Route path="/profile" element={<UserProfile />} />
+              <Route path="/how" element={<About />} />
+            </>
+          ) : (
+            <Route path="/" element={<Container />} />
+          )}
 
-        <Route
-          path="*"
-          element={
-            <div className="container t-center ">
-              <h1 className="header-1 p-50">404 - Page not found</h1>
-              <p>This page does not exist or requires to log in.</p>
-            </div>
-          }
-        />
-      </Routes> }
-
-      </Routes>
+          <Route
+            path="*"
+            element={
+              <div className="container t-center ">
+                <h1 className="header-1 p-50">404 - Page not found</h1>
+                <p>This page does not exist or requires to log in.</p>
+              </div>
+            }
+          />
+        </Routes>
+      }
       <Footer />
-
     </>
   );
 };
