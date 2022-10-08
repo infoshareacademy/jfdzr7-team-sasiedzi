a269aa683bdc7d92d92355c9a56dedd710887f95
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2022-10-08 13:53:39 +0200

    hash router dodany

--- neighbors/src/index.js
+++ neighbors/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { ThemeProvider } from './helpers/apiCommunication';
 import './style/reset.css';
@@ -11,10 +11,10 @@ import { App } from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
