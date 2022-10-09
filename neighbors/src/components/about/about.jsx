26a34d70ab2cd36ca60abf6538a827c1933c9031
Anna Kierlin <ania.kierlin@gmail.com>
2022-10-09 08:54:33 +0200

    Poprawa tytułu about

--- neighbors/src/components/about/about.jsx
+++ neighbors/src/components/about/about.jsx
@@ -11,7 +11,7 @@ export const About = () => {
             <FontAwesomeIcon icon={faCircleInfo} className="icon" />
           </div>
           <div className="right">
-            <h1 className="about-header">How it works?</h1>
+            <h1 className="about-header">How doeas it work? </h1>
             <p className="about-paragraph">
               The Neighbors app was created to communicate with people around us. Here you can post announcements in
               which you will offer your help, but also get it! All you have to do is complete your profile, add a post
